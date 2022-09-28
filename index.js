b3477a43b7560abc9e16b408ef885536a45dd6d3
DevXprite <devxprite@gmail.com>
2022-09-28 14:08:00 +0530

    npm run format

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-const { default: axios } = require('axios');
+const { default: axios } = require("axios");
 const Bluebird = require("bluebird");
-const { prompt } = require('enquirer');
-const chalk = require('chalk');
-const ora = require('ora');
-const figlet = require('figlet');
-const pkg = require("./package.json")
-
+const { prompt } = require("enquirer");
+const chalk = require("chalk");
+const ora = require("ora");
+const pkg = require("./package.json");
 
 const instance = axios.create({
     validateStatus: (status) => status >= 0 && status <= 1000,
 });
 
-const banner = (name) => {
-   console.log(`
-   
-        ${chalk.green(`v${pkg.version}`)}
-    `);
-}
+const banner = () => {
+    // console.log(`
 
-const input = (inputName) => {
-    return new Promise((resolve) => {
-        prompt({
-            type: 'input',
-            name: inputName,
-            message: `Enter a Phone Number or UPI `
-        }).then((data) => {
-            console.log('\n')
-            resolve(data[inputName])
-        })
-    })
-}
+    //     ${chalk.green(`v${pkg.version}`)}
+    // `);
+};
+
+const input = (inputName) => new Promise((resolve) => {
+    prompt({
+        type: "input",
+        name: inputName,
+        message: "Enter a Phone Number or UPI ",
+    }).then((data) => {
+        console.log("\n");
+        resolve(data[inputName]);
+    });
+});
 
 const banks = [
     "apl",
@@ -49,43 +45,38 @@ const banks = [
     "okaxis",
     "dbs",
     "barodampay",
-    "idfcbank"
-]
+    "idfcbank",
+];
 
-
-const checkUpi = async (upi) => {
-    return new Promise((resolve) => {
-        let spinner = ora(`Checking ${upi}`).start();
-        instance.post(`https://upibankvalidator.com/api/upiValidation?upi=${upi}`).then((response) => {
-            let data = response.data;
-            if (data.isUpiRegistered) {
-                spinner.succeed(`UPI ${chalk.cyanBright(upi)} is registered to ${chalk.magentaBright(data.name)}.`)
-            } else {
-                spinner.fail(`UPI ${chalk.cyanBright(upi)} is not yet registered to any person or entity.`)
-            }
-            resolve();
-        })
-    })
-}
+const checkUpi = async (upi) => new Promise((resolve) => {
+    const spinner = ora(`Checking ${upi}`).start();
+    instance.post(`https://upibankvalidator.com/api/upiValidation?upi=${upi}`).then((response) => {
+        const { data } = response;
+        if (data.isUpiRegistered) {
+            spinner.succeed(`UPI ${chalk.cyanBright(upi)} is registered to ${chalk.magentaBright(data.name)}.`);
+        } else {
+            spinner.fail(`UPI ${chalk.cyanBright(upi)} is not yet registered to any person or entity.`);
+        }
+        resolve();
+    });
+});
 
 (async () => {
+    banner();
 
-    banner()
-    
-    const inputUPI = await input('UPI');
+    const inputUPI = await input("UPI");
 
-    const UPIs = banks.map((bank) => {
-        return inputUPI.replace(/\s/g, '').split('@')[0] + '@' + bank;
-    })
+    const UPIs = banks.map((bank) => `${inputUPI.replace(/\s/g, "").split("@")[0]}@${bank}`);
 
-    await Bluebird.map(UPIs, (upi) => {
-        return new Promise((resolve) => {
+    await Bluebird.map(
+        UPIs,
+        (upi) => new Promise((resolve) => {
             checkUpi(upi).then(() => {
-                resolve()
-            })
-        })
-    },
-        { concurrency: 1 })
+                resolve();
+            });
+        }),
+        { concurrency: 1 },
+    );
 
     console.log(chalk.greenBright(`\n\nThank You! for using ${pkg.name}.`));
-})()
+})();
